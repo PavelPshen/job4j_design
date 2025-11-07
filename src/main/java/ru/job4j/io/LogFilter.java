4fc9a9a2045028ff614bf37de929752a9aa9d58f
PavelPshen <pshen-leti@mail.ru>
2025-11-08 00:09:58 +0300

    5.1. BufferedReader [#252489 #554442]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,7 +2,6 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class LogFilter {
@@ -15,32 +14,20 @@ public class LogFilter {
     public List<String> filter() {
         List<String> result = new ArrayList<>();
         try (BufferedReader input = new BufferedReader(new FileReader(file))) {
-          result = input.lines().
-                  filter(p -> p.contains(" 404 ")).
-                  toList();
+            for (String line : input.lines().toList()) {
+                String[] find404 = line.split(" ");
+                if ("404".equals(find404[find404.length - 2])) {
+                    result.add(line);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return result;
     }
 
-    public void saveTo(String out) {
-        var data = filter();
-        try (PrintWriter output = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(out)
-                ))) {
-            for (String lines : data) {
-                output.printf("%s%n", lines);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter("data/log.txt");
         logFilter.filter().forEach(System.out::println);
-        new LogFilter("data/log.txt").saveTo("data/404.txt");
     }
 }
