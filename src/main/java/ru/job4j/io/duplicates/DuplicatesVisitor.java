1152b28888dbcf5f7d66e6499dcb2ad3a9e6b25a
PavelPshen <pshen-leti@mail.ru>
2025-11-13 21:01:51 +0300

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,20 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+
+    @Override
+    public FileVisitResult visitFile(Path file,
+                                     BasicFileAttributes attributes) throws IOException {
+        System.out.println(file.toAbsolutePath());
+        FileProperty fileProperty = new FileProperty(attributes.size(), file.toString());
+        System.out.println(fileProperty.getSize());
+        System.out.println(fileProperty.getName());
+        return super.visitFile(file, attributes);
+    }
+}
